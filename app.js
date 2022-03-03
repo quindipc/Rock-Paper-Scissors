a6ef1552540b9eb8f0a13c71f45b1a05d42c44d2
Chelsea Quindipan <quindipc@gmail.com>
2022-03-03 14:50:12 -0500

    initial

---
+++ app.js
@@ -0,0 +1,6 @@
+//PSEUDO CODE -- User should be able to click rock, paper, or scissors and the computer's choice should be generated
+
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+
